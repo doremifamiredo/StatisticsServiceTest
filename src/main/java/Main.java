78512e3c1f3914ed91fd360e9c4ec0f262bc803e
Oleg Somov <doremi@fasollasi.ru>
2024-06-16 19:46:09 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import ru.netology.statistic.StatisticsService;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StatisticsService service = new StatisticsService();
+    }
+
+}
